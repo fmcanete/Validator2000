d9a6cdf4187e9fee8bb81b3b2448e324add1058b
fmcanete <62968006+fmcanete@users.noreply.github.com>
2020-11-12 02:16:43 -0300

    FIX. Bucle fuera de rango
    se corrige el bucle que itera el MOV2000. El cual quedaba fuera de rango. Se agrega un salto de linea en el metodo que carga la listaArchivo en el mov2000

--- archivos.py
+++ archivos.py
@@ -17,66 +17,68 @@ class manejoDeLosArchivosTXT():
 		contadorArchivo = 0
 		listaArchivo = []
 		for linea in mov2000Plano:
-			if linea[-1] == "\n":			#si el ultimo caracter es salto de linea
+			if linea[0] != "":				#si el primer caracter es distinto de vacio
 				linea = linea[:-1]			#desde el comienzo hasta el final lo agrega
 				listaArchivo.append(linea)
+				print("linea", linea)
 				contadorArchivo = contadorArchivo+1
-		#		print("LINEA: ",contadorArchivo, listaArchivo)
-			else:
-				listaArchivo.append(linea)
-		#		print("LINEA: ",contadorArchivo, listaArchivo)
+				#print("LINEA: ", listaArchivo[contadorArchivo])
+			pass
+		listaArchivo.append("")
+		print("LINEA 2: ",listaArchivo) 					#SE AGREGA SALTO DE LINEA
 		return listaArchivo, contadorArchivo
 
 	def subStringLista(listaArchivo, contadorArchivo, listaCompleta):
-		cont = 0
-		numTarMovMes = []
-		establecimiento = []
-		nroAut =[]
-		planCuot =[]
-		numCuot =[]
-		moneda = []
-		importe = []
-		codPais = []
-		importeOrig = []
-		binTarj = []
-		nombreComercio = []
-		bancoEstablecimiento = []
-		numTarMov2000 = []
-		token = []
-		numToken = []
-		posDataCode = []
-		visaRelease = []
-		saltoLinea = []
-		while listaArchivo[cont] != "":
-			cadena = listaArchivo[cont]
-			cont = cont+1
-			if cadena[0] == "D":
-				numTarMovMes.append(cadena[35:51])
-				establecimiento.append(cadena[51:61])
-				nroAut.append(cadena[111:119])
-				planCuot.append(cadena[127:129])
-				numCuot.append(cadena[129:131])
-				moneda.append(cadena[131:134])
-				importe.append(str(float(cadena[134:149])/100))
-				codPais.append(cadena[153:155])
-				importeOrig.append(cadena[156:171])
-				binTarj.append(cadena[171:177])
-				nombreComercio.append(cadena[321:342])
-				bancoEstablecimiento.append(cadena[396:399])
-				numTarMov2000.append(cadena[440:456])
-				token.append(cadena[535])
-				numToken.append(cadena[536:552])
-				posDataCode.append(cadena[781:794])
-				visaRelease.append(cadena[1059])
-				saltoLinea.append(cadena[1053])
-				data = {'numTarMovMes': numTarMovMes, 'establecimiento':establecimiento, 'nroAut': nroAut, 'planCuot': planCuot,
-				'numCuot': numCuot, 'moneda': moneda, 'importe': importe, 'codPais': codPais, 'importeOrig': importeOrig, 'binTarj':binTarj,
-				'nombreComercio': nombreComercio, 'bancoEstablecimiento': bancoEstablecimiento, 'numTarMov2000': numTarMov2000, 'token':token,
-				'posDataCode':posDataCode, 'visaRelease':visaRelease}
-				df = pd.DataFrame(data, columns = ['numTarMovMes', 'establecimiento', 'nroAut','planCuot','numCuot', 'moneda',
-				'importe', 'codPais','importeOrig', 'binTarj', 'nombreComercio', 'bancoEstablecimiento',
-				'numTarMov2000', 'Token', 'numToken', 'posDataCode', 'visaRelease'])
+			cont = 0
+			numTarMovMes = []
+			establecimiento = []
+			nroAut =[]
+			planCuot =[]
+			numCuot =[]
+			moneda = []
+			importe = []
+			codPais = []
+			importeOrig = []
+			binTarj = []
+			nombreComercio = []
+			bancoEstablecimiento = []
+			numTarMov2000 = []
+			token = []
+			numToken = []
+			posDataCode = []
+			visaRelease = []
+			saltoLinea = []
+			while listaArchivo[cont] != "":
+				cadena = listaArchivo[cont]
+				cont = cont+1
+				if cadena[0] == "D":
+					numTarMovMes.append(cadena[35:51])
+					establecimiento.append(cadena[51:61])
+					nroAut.append(cadena[111:119])
+					planCuot.append(cadena[127:129])
+					numCuot.append(cadena[129:131])
+					moneda.append(cadena[131:134])
+					importe.append(str(float(cadena[134:149])/100))
+					codPais.append(cadena[153:155])
+					importeOrig.append(cadena[156:171])
+					binTarj.append(cadena[171:177])
+					nombreComercio.append(cadena[321:342])
+					bancoEstablecimiento.append(cadena[396:399])
+					numTarMov2000.append(cadena[440:456])
+					token.append(cadena[535])
+					numToken.append(cadena[536:552])
+					posDataCode.append(cadena[781:794])
+					visaRelease.append(cadena[1059])
+					saltoLinea.append(cadena[1053])
+					data = {'numTarMovMes': numTarMovMes, 'establecimiento':establecimiento, 'nroAut': nroAut, 'planCuot': planCuot,
+					'numCuot': numCuot, 'moneda': moneda, 'importe': importe, 'codPais': codPais, 'importeOrig': importeOrig, 'binTarj':binTarj,
+					'nombreComercio': nombreComercio, 'bancoEstablecimiento': bancoEstablecimiento, 'numTarMov2000': numTarMov2000, 'token':token,
+					'posDataCode':posDataCode, 'visaRelease':visaRelease}
+					df = pd.DataFrame(data, columns = ['numTarMovMes', 'establecimiento', 'nroAut','planCuot','numCuot', 'moneda',
+					'importe', 'codPais','importeOrig', 'binTarj', 'nombreComercio', 'bancoEstablecimiento',
+					'numTarMov2000', 'Token', 'numToken', 'posDataCode', 'visaRelease'])
+				pass
 			pass
-		pass
-		df.to_csv('CSV_MOV2000.csv')
-		return listaCompleta
+			df.to_csv('CSV_MOV2000.csv')
+			return listaCompleta
+		
