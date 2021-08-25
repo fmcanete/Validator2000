a13fa686fcc37005603e2c7ad428b70d6962c55a
Jose <jabmjose@gmail.com>
2021-08-25 18:32:44 -0300

    Modificacion BDD

--- mcap/conectionMCAP.py
+++ mcap/conectionMCAP.py
@@ -2,20 +2,44 @@
 
 import pyodbc
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox,filedialog
+import configparser
+
+def abrir_archivo():
+    archivo_abierto=filedialog.askopenfilename(initialdir = "/", #esto abre en el raiz. ver de mejorar y poner donde esta el proyecto
+    title = "Seleccione archivo",filetypes = (("txt files","*.txt"),
+    ("all files","*.*")))
+    return(archivo_abierto)
 
 
 def llamado():
+    config = configparser.ConfigParser()
+    config.read('mcap\\Aconfig.ini')
 
-    server = 'wapp-ttpm025'
-    bd = 'CapturaStratus'
-    usuario = 'Captura'
-    contrasena = 'Captura'
+    server = config['DEFAULT']['SERVER_NAME']
+    bd = config['DEFAULT']['DB_NAME']
+    usuario = config['DEFAULT']['DB_USER']
+    contrasena = config['DEFAULT']['DB_PASSWORD']
+
+    ruta = abrir_archivo()
+    elbulk = 'BULK INSERT CapturaStratus.dbo.MOV2000_V1 FROM' + " '" + ruta + "'"
+
 
     try:
         conexion = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+bd+';UID='+usuario+';PWD='+contrasena+'')
         print("Conexion OK")
-        messagebox.showinfo(message="¡Conexión OK!", title="OK")
+        cursor = conexion.cursor()
+        cursor.execute('TRUNCATE TABLE CapturaStratus.dbo.MOV2000_V1')
+        cursor.execute(elbulk)
+        
+        conexion.commit()
+        conexion.close()
+
+
+        messagebox.showinfo(message='¡Subida OK al '+server+', verificar MOV2000_V1 en CapturaStratus!', title="OK")
+        #conexion.close()
+        #cursor.close()
+    
     except :
         messagebox.showinfo(message="¡Fallo en Conexión!", title="Error")
         print("Fallo Conexion")
